e8401e5c5b9e3bb2a36611d36017b65da03321c2
Pieter de Jong <pieter.a.dejong@gmail.com>
2021-05-28 10:55:02 -0400

    added stuff

---
+++ count_to_n.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int count;
+  for(count = 1; count < 11; count++) {
+  	printf("my number: %d", count);
+  	printf("\n");
+  }
+}
+
---
+++ linked_list.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct test_strut {
+	int val;
+	struct test_struct *next;
+};
+
